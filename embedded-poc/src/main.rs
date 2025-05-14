28c331aff400f9396563cfa1625fde1186164d6d
mad201802 <md148@hdm-stuttgart.de>
2025-05-14 20:34:41 +0000

    feat: update dependencies and enhance esp main function with async support

--- embedded-poc/src/main.rs
+++ embedded-poc/src/main.rs
@@ -1,21 +1,15 @@
 use std::net::Ipv4Addr;
 
+use anyhow::Result;
 use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::prelude::Peripherals, ipv4};
+use esp_idf_sys::esp;
 use eth::start_eth;
 use protocol::sd::ServiceDiscovery;
+use tokio::runtime::{self, Runtime};
 
 mod eth;
 
-fn main() -> anyhow::Result<()> {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
-
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
-
-    log::info!("Hello, world!");
-
+async fn _main() -> Result<()> {
     let p = Peripherals::take()?;
     let pins = p.pins;
     let sys_loop = EspSystemEventLoop::take()?;
@@ -48,16 +42,41 @@ fn main() -> anyhow::Result<()> {
     );
 
     let service_id = 2;
-
     let timeout = 5;
 
     let mut sd = ServiceDiscovery::new(service_id);
-    sd.init()?;
-    sd.start()?;
-    println!("Service Discovery started with ID: {}, Timeout: {}", service_id, timeout);
+    sd.init().await?;
+    sd.start().await?;
+    println!(
+        "Service Discovery started with ID: {}, Timeout: {}",
+        service_id, timeout
+    );
 
     std::thread::sleep(std::time::Duration::from_secs(timeout));
 
     println!("Service Discovery finished after {} seconds", timeout);
+
+    Ok::<(), anyhow::Error>(())
+}
+
+fn main() -> Result<()> {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    let config = esp_idf_sys::esp_vfs_eventfd_config_t {
+        max_fds: 1,
+        ..Default::default()
+    };
+    esp! { unsafe { esp_idf_sys::esp_vfs_eventfd_register(&config) } }?;
+
+    runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?
+        .block_on(_main())?;
+
     Ok(())
 }

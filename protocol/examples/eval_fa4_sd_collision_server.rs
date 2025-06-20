448d75b73d0d7ce87195a8992a0a933075f9ffef
mad201802 <md148@hdm-stuttgart.de>
2025-06-20 16:52:05 +0000

    feat: add Docker Compose configuration and example applications for service discovery client and server

---
+++ protocol/examples/eval_fa4_sd_collision_server.rs
@@ -0,0 +1,18 @@
+use std::{thread::{self}, time::Duration};
+
+use anyhow::Result;
+use protocol::sd::{ServiceDiscovery, ServiceDiscoveryInterface};
+
+const CURRENT_SERVICE_ID: u16 = 0x01;
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let mut sd = ServiceDiscovery::new(CURRENT_SERVICE_ID);
+    sd.init()?;
+    sd.start()?;
+
+    thread::sleep(Duration::from_secs(5));
+    
+    Ok(())
+}
